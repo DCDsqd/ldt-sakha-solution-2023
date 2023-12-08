099ee7e096c52ee0400333e45f89c34ccae18aaa
storst1 <kokakolazzz100@mail.ru>
2023-12-09 01:56:16 +0300

    rename to tokens

--- backend/parser/yt_parser.py
+++ backend/parser/yt_parser.py
@@ -5,7 +5,7 @@ import googleapiclient.errors
 
 # Disable OAuthlib's HTTPS verification when running locally.
 # DO NOT leave this option enabled in production.
-os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "0"
 
 api_service_name = "youtube"
 api_version = "v3"
@@ -15,8 +15,7 @@ client_secrets_file = "../../secrets/client_secrets.json"
 flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
     client_secrets_file, scopes=["https://www.googleapis.com/auth/youtube.readonly"])
 credentials = flow.run_console()
-youtube = googleapiclient.discovery.build(
-    api_service_name, api_version, credentials=credentials)
+youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
 
 request = youtube.subscriptions().list(
     part="snippet",

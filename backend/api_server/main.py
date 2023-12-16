8498c1aebf7c035dbb04607dd580b4c394308988
storst1 <kokakolazzz100@mail.ru>
2023-12-16 04:32:58 +0300

    chto

--- backend/api_server/main.py
+++ backend/api_server/main.py
@@ -2,16 +2,22 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import uvicorn
 import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from nltk.corpus import stopwords
 
 from parser.yt_parser import init_youtube_with_user_token, get_user_yt_subscriptions, get_user_liked_videos, \
-YTChannel, YTVideoInfo
+    YTChannel, YTVideoInfo
 from parser.common import clean_text_for_model
 
 
 app = FastAPI()
-model = pickle.load(open('models/text_model.sav', 'rb'))
+text_model = pickle.load(open('models/text_model.sav', 'rb'))
+loaded_tfidf_vectorizer = pickle.load(open('models/text_model_tfidf_vectorizer.pkl', 'rb'))
+multi_label_binarizer = pickle.load(open('models/text_model_mlb.pkl', 'rb'))
 
 
+# Class that represents API input structure
 class InputData(BaseModel):
     yt_token: str
     vk_token: str
@@ -40,10 +46,20 @@ def predict(input_data: InputData):
             united_text = " ".join([video.concatenate_text() for video in channel_videos])
             united_text = clean_text_for_model(united_text)
 
-        prediction = model.predict([input_data.yt_token])
+            text_model.predict()
+
+            # Получение предсказаний вероятности
+            predicted_probabilities = text_model.predict_proba(transformed_text)
+
+            # Преобразование вероятностей в список для JSON сериализации
+            probabilities_list = predicted_probabilities.tolist()
+
+            # Получение названий классов
+            classes_list = mlb.classes_.tolist()
+
         return {
-            "predictions": prediction.tolist(),
-            "predictions_score": prediction.tolist(),
+            "predictions_score": probabilities_list,
+            "predictions_classes": classes_list,
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))

3bd0922397d3496052bbff2409f1ba47e4fe0120
storst1 <kokakolazzz100@mail.ru>
2023-12-17 18:29:40 +0300

    partially add bert to ease up deploy

--- backend/test/test_post.py
+++ backend/test/test_post.py
@@ -3,9 +3,9 @@ import json
 
 url = 'http://127.0.0.1:8000/predict'
 data = {
-    "yt_token": "ya29.a0AfB_byDoGvI81nOJ0VCqZNuvO_S66rpNzJoNln9fg8SiLpGgA18gTyfV2ioDexyEshz-rCQgL7L8E8uKrT6JhWHP6hSbMEvOZzUhYV2wO2e-SsnuQUwgwqgkcf1OsTPHOil54sR70jd17oGbiovfjqyLi0imtxW_F07HaCgYKASYSARMSFQHGX2MifUY2TJzd_QT7w6DtAWubJw0171",
+    "yt_token": "ya29.a0AfB_byCxS6UH98Ne6ym2rN50LAnf6aGWEiAgDrVZKT7L4MpAK9XNR6D_ll24-LREMjFw767EhOa-7XnmnzXzQ1I3DGXusNCifRbg073RMvyHAlJQel-qyM0cFk5IDpOIDisLlLTaQBIHqtbXYRUB0xWBkwHNx8GX3wusaCgYKAWwSARMSFQHGX2MiAA9NFKpsUDxGaMKYU4ezfA0171",
     "vk_token": "",
-    "debug_text": ""
+    "tg_posts": []
 }
 
 response = requests.post(url, json=data)
